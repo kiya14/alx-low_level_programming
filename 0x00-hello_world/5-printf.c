9536cce59342db8d13e0390df318a36310e0b34e
mulualem <mulualemgiragn3@gmail.com>
2022-04-07 15:00:19 -0700

    with print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ *main - entry point
+ * My hello, printf task
+ *Return: return 1 after program excution
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n"));
+return (0);
+}
