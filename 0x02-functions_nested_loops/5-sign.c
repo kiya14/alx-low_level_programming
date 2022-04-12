bd26cba08d3d5b53d86410f504e8cbd0616f9300
mulualem <mulualemgiragn3@gmail.com>
2022-04-12 14:18:49 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,16 +2,7 @@
 
 /**
  * print_sign - prints the sign of a number.
- * @c: The number of which the sign will be printed.
- *
- * Return: 1 if the number is grater than zero,
- *         0nif the number is zero'
- *         root@7034b366295a:~/alx-low_level_programming/0x02-functions_nested_loops# cat > 5-sign.c
-#include "main.h"
-
-/**
- * print_sign - prints the sign of a number.
- * @c: The number of which the sign will be printed.
+ * @n: The number of which the sign will be printed.
  *
  * Return: 1 if the number is grater than zero,
  *         0 if the number is zero,
@@ -26,7 +17,7 @@ return (1);
 }
 else if (n == 0)
 {
-_putchar('0');
+_putchar('0);
 return (0);
 }
 else
