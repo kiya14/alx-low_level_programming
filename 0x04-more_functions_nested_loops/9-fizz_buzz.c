532c8d2fc5750a212d9fb737bd540420bcca8cea
mulualem <mulualemgiragn3@gmail.com>
2022-04-14 12:51:40 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#includ "main.h"
+
+/**
+ * main - prints Fizz for int divisible by 3 and Buzz for 5
+ *
+ * Return: int
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf("Fizz");
+}
+else if (i % 3 != 0 && i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+if ( i != 100)
+_putchar(' ');
+else
+_putchar('\n');
+}
+return (0);
+}
