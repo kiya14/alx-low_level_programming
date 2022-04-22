e4cbad71ad059c985bee2ee61237227e5b96d7e1
mulualem <mulualemgiragn3@gmail.com>
2022-04-22 14:10:32 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,44 +1,24 @@
 #include "main.h"
 /**
-  * _strcmp - a function that compares two strings
- *
- * @s1: string 1 input to compare
- * @s2: against this other string 2
- *
- * Return: 0 if s1 and s2 are equal
- * negative integer if the stopping character
- * in @s1 was less than the stopping
- * character in @s2
- * positive integer if the stopping character
- *  in @s1 was greater than the stopping
- * character in @s2
+ *_strcmp - compare two strings
+ *@s1: A pointer to a character that will be changed
+ *@s2: A pointer to a character that will also be changed/modified/updated
+ *Return: dest
  */
 
 int _strcmp(char *s1, char *s2)
+
 {
-  int i = 0, r;
 
-while (s1[i] != '\0' && s2[i] != '\0')
-  {
-    /**
-     * if they differ by a single character
-     * don't iterate further
-    */
-    if (s1[i] != s2[i])
-    {
-      /*
-       * return the difference between
-       * the two characters
-      */
-      r = s1[i] - s2[i];
-      break;
-    }
-    else
-    {
-      r = s1[i] - s2[i];
-    }
-    i++;
-  }
+char *str_one = s1;
+char *str_two = s2;
 
-  return (r);
+
+while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
+{
+str_one++;
+str_two++;
+}
+
+return (*str_one - *str_two);
 }
