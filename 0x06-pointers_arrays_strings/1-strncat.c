236af873e49f1b4076ebb45055aa7b59727c5fc3
mulualem <mulualemgiragn3@gmail.com>
2022-04-21 19:08:15 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
  * _strncat - like _strcat but it will use n bytes from src
-
-
  * @dest: string to be concatenated
  * @src: string to be concatenated until n limit
  * @n: number of bytes that will be used from src
